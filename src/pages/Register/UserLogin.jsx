12de4ec068bf293f113930b6f02f8303cdd9b5db
Gideon Sowah <gideonsowah9@gmail.com>
2025-08-18 14:57:14 +0000

    change login url

--- src/pages/Register/UserLogin.jsx
+++ src/pages/Register/UserLogin.jsx
@@ -10,7 +10,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import api from "@/services/axios";
+import axios from "axios";
 
 const UserLogin = () => {
   const [password, setpassword] = useState("");
@@ -26,7 +26,7 @@ const UserLogin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = { password, email };
-    const res = await api.post(
+    const res = await axios.post(
       `https://fellowship.onrender.com/login`,
       formData,
     );
