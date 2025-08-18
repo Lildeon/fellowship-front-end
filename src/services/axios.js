0f1e3275dbfec954251c4b7d6037bdc90f485773
Gideon Sowah <gideonsowah9@gmail.com>
2025-08-18 17:36:10 +0000

    add headers to axios instance

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://fellowship-backend.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials: true,
 });
 export default api;
