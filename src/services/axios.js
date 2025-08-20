f89c723d8d083b80e34e129349171b17f3498a21
Gideon Sowah <gideonsowah9@gmail.com>
2025-08-20 07:57:37 +0000

    remove content-type

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://fellowship-backend.up.railway.app",
-  headers: {
-    "Content-Type": "application/json",
-  },
+
   withCredentials: true,
 });
 export default api;
