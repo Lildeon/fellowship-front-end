7ab74029a68d66fe50fb567d36775e0313c75a41
Gideon Sowah <gideonsowah9@gmail.com>
2025-09-11 18:17:13 +0000

    configure react-query

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 2,
+      staleTime: 1000 * 60, // 1 minute fresh
+      cacheTime: 1000 * 60 * 5, // keep inactive queries for 5 mins
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
