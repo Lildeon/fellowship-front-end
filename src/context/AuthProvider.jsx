3b5813c47b19fb8fde699d0c6fd75537007d2763
Gideon Sowah <gideonsowah9@gmail.com>
2025-09-14 19:44:38 +0000

    delete lines of localstorage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -17,9 +17,6 @@ export const AuthProvider = ({ children }) => {
 
   const logout = async () => {
     localStorage.removeItem("user");
-    localStorage.removeItem("currentPage");
-    localStorage.removeItem("pageNumber");
-    localStorage.removeItem("pageCount");
     await axios.post("https://fellowship-backend.up.railway.app/logout", {
       withCredentials: true,
     });
