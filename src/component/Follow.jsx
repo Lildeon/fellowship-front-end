f8dd0b2fbc5cabead93a6e3eec03ea51c331623e
Gideon Sowah <gideonsowah9@gmail.com>
2025-09-18 14:57:06 +0000

    add optimistic update with react query

--- src/component/Follow.jsx
+++ src/component/Follow.jsx
@@ -1,15 +1,56 @@
 /* eslint-disable react/prop-types */
 import api from "@/services/axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const Follow = ({ userId, followed, onToggle }) => {
+const Follow = ({ user }) => {
+  const currentUserId = localStorage.getItem("user");
+  const queryClient = useQueryClient();
   const handleFollow = async () => {
-    await api.post(`follow/${userId}`);
-    onToggle();
+    await api.post(`follow/${user._id}`);
   };
+  const mutation = useMutation({
+    mutationFn: handleFollow,
+    onMutate: async (userTofollowId) => {
+      await queryClient.cancelQueries({ queryKey: ["user", userTofollowId] });
+      const prevData = queryClient.getQueryData(["user", userTofollowId]);
+      queryClient.setQueryData(["user", userTofollowId], (old) => {
+        if (!old) return old;
+        const alreadyFollowing = old.follower.includes(currentUserId);
+        return {
+          ...old,
+
+          follower: alreadyFollowing
+            ? old.follower.filter((id) => id._id !== currentUserId)
+            : [...old.likes, currentUserId],
+        };
+      });
+      return { prevData };
+    },
+    onError: (_err, _vars, context) => {
+      // rollback on error
+      queryClient.setQueryData(
+        ["user", context.prevData._id],
+        context.prevData,
+      );
+    },
+
+    onSettled: (_data, _err, { userTofollowIdUserId }) => {
+      // refetch to sync with server
+      queryClient.invalidateQueries(["user", userTofollowIdUserId], {
+        refetchType: "inactive",
+      });
+    },
+  });
 
   return (
-    <button type="button" onClick={handleFollow}>
-      {followed?.followed ? followed?.followed : followed?.followed}
+    <button type="button" onClick={() => mutation.mutate(user._id)}>
+      {user?.follower?.includes(currentUserId) ? (
+        <p className="px-5 py-1 h-fit w-28 rounded-2xl border">following</p>
+      ) : (
+        <p className="px-5 py-1 h-fit w-28 rounded-2xl bg-black text-white">
+          follow
+        </p>
+      )}
     </button>
   );
 };
--- src/component/FollowPage.jsx
+++ src/component/FollowPage.jsx
@@ -1,15 +1,51 @@
 /* eslint-disable react/prop-types */
 import api from "@/services/axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const FollowPage = ({ pageId, url, followed, onToggle }) => {
+const FollowPage = ({ page, url }) => {
+  const currentUserId = localStorage.getItem("user");
+  const queryClient = useQueryClient();
   const handleFollow = async () => {
-    await api.post(`${url}/${pageId}`);
-    onToggle();
+    await api.post(`${url}/${page._id}`);
   };
 
+  const mutation = useMutation({
+    mutationFn: handleFollow,
+    onMutate: async (pageTofollowId) => {
+      await queryClient.cancelQueries({ queryKey: ["page", pageTofollowId] });
+      const prevData = queryClient.getQueryData(["page", pageTofollowId]);
+      queryClient.setQueryData(["page", pageTofollowId], (old) => {
+        if (!old) return old;
+        const alreadyFollowing = old.followers.includes(currentUserId);
+        return {
+          ...old,
+
+          followers: alreadyFollowing
+            ? old.followers.filter((id) => id._id !== currentUserId)
+            : [...old.likes, currentUserId],
+        };
+      });
+      return { prevData };
+    },
+    onError: (_err, _vars, context) => {
+      // rollback on error
+      queryClient.setQueryData(
+        ["page", context.prevData._id],
+        context.prevData,
+      );
+    },
+
+    onSettled: (_data, _err, { pageTofollowId }) => {
+      // refetch to sync with server
+      queryClient.invalidateQueries(["page", pageTofollowId], {
+        refetchType: "inactive",
+      });
+    },
+  });
+
   return (
-    <button type="button" onClick={handleFollow}>
-      {followed?.followed === "following" ? (
+    <button type="button" onClick={() => mutation.mutate(page._id)}>
+      {page?.followers.includes(currentUserId) ? (
         <p
           className="px-5 py-1 h-fit w-28
     rounded-2xl border"
